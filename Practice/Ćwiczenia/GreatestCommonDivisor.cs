298e6243a4f9eeb3655915e5b2e53dda7b380281
KonradChyrzynski <konrad.chyrzynskii@gmail.com>
2022-10-19 19:27:26 +0200

    Gratest common divisor eucalidean algo

---
+++ Practice/Ćwiczenia/GreatestCommonDivisor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ćwiczenia
+{
+    public class GreatestCommonDivisor
+    {
+        public int gcp_euclidean(int a, int b)
+        {
+            if (b != 0)
+            {
+                return gcp_euclidean(b, a % b);
+            }
+            return a;
+        }
+    }
+}

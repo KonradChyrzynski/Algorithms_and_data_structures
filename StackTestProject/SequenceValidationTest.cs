095f4700253ea5faf87ad42bda959851bd402401
KonradChyrzynski <konrad.chyrzynskii@gmail.com>
2022-11-17 18:37:29 +0100

    sequence validation algo

---
+++ StackTestProject/SequenceValidationTest.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using Xunit;
+
+namespace StackTestProject
+{
+    public class SequenceValidationTest
+    {
+        [Fact]
+        public void checkIfSequenceValidationMethodWorksCorrectly()
+        {
+            bool test1true = validateSequenceOfSpecialCharacters("([()])");
+            bool test1false = validateSequenceOfSpecialCharacters("([([)])");
+            bool test2true = validateSequenceOfSpecialCharacters("([([])])");
+            bool test2false = validateSequenceOfSpecialCharacters("((]]");
+            bool test3true = validateSequenceOfSpecialCharacters("[()]");
+            bool test3false = validateSequenceOfSpecialCharacters("[[))");
+
+            Assert.True(test1true);
+            Assert.True(test2true);
+            Assert.True(test3true);
+            Assert.False(test1false);
+            Assert.False(test2false);
+        }
+
+        private static bool validateSequenceOfSpecialCharacters(string sequence)
+        {
+            Stack<string> stack = new Stack<string>();
+            Dictionary<char, string> open = new Dictionary<char, string>() { { char.Parse("("), "circular" }, { char.Parse("["), "rectangular" } };
+            Dictionary<char, string> close = new Dictionary<char, string>() { { char.Parse(")"), "circular" }, { char.Parse("]"), "rectangular" } };
+
+            foreach (char letter in sequence)
+            {
+                if (open.ContainsKey(letter))
+                {
+                    stack.Push(open[letter]);
+                }
+                else if (close.ContainsKey(letter) && stack.Count != 0)
+                {
+                    if (stack.Peek() == close[letter])
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (stack.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}

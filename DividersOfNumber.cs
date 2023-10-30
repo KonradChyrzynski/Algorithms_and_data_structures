127cf9f3301fa9509bdb23302791b2bf8f585fc2
konrad.chyrzynski <konrad.chyrzynskii@gmail.com>
2023-10-30 22:57:30 +0100

    Added new solutions

---
+++ DividersOfNumber.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace AlghoritmsAndDataStructures
+{
+    public class DividersOfNumber
+    {
+        public void printDividersOfNumbersSync()
+        {
+            foreach (int number in findDividersOfNumberSync(5))
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public static List<int> findDividersOfNumberSync(int number) 
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+
+        public static List<int> findDividersOfNumberSquare(int number) 
+        {
+            List<int> result = new List<int>();
+            for(int i = 1; i <= Math.Sqrt(number); i++) 
+            { 
+                if(number % i == 0) 
+                { 
+                    if (number / i == i) 
+                    {
+                        result.Add(i);
+                    }
+                    else 
+                    {
+                        result.Add(number / i);
+                        result.Add(i);
+                    }
+                }
+            }
+            return result;
+        }
+
+        [Benchmark]
+        public List<int> findDivisorsSync() => findDividersOfNumberSync(36);
+        [Benchmark]
+        public List<int> findDivisorsSquare() => findDividersOfNumberSquare(36);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿using DotNetty.Common.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AlghoritmsAndDataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int failed = 0;
+            var tasks = new List<Task>();
+
+            String[] urls = { "www.adatum.com", "www.cohovineyard.com",
+                        "www.cohowinery.com", "www.northwindtraders.com",
+                        "www.contoso.com" };
+
+            foreach (var value in urls)
+            {
+                var url = value;
+                tasks.Add(Task.Run(() => {
+                    var png = new Ping();
+                    try
+                    {
+                        var reply = png.Send(url);
+                        if (!(reply.Status == IPStatus.Success))
+                        {
+                            Interlocked.Increment(ref failed);
+                            throw new TimeoutException("Unable to reach " + url + ".");
+                        }
+                    }
+                    catch (PingException)
+                    {
+                        Interlocked.Increment(ref failed);
+                        throw;
+                    }
+                }));
+            }
+            Task t = Task.WhenAll(tasks);
+            try
+            {
+                t.Wait();
+            }
+            catch { }
+
+            if (t.Status == TaskStatus.RanToCompletion)
+                Console.WriteLine("All ping attempts succeeded.");
+            else if (t.Status == TaskStatus.Faulted)
+                Console.WriteLine("{0} ping attempts failed", failed);
+
+        }
+
+        public static IEnumerable<List<char>> SplitList<T>(List<char> locations, int nSize)
+        {
+            for (int i = 0; i < locations.Count; i += nSize)
+            {
+                yield return locations.GetRange(i, Math.Min(nSize, locations.Count - i));
+            }
+        }
+
+        static List<int[]> FindDuplicatedProduct(int[] arr)
+        {
+            Dictionary<int, List<int[]>> map = new Dictionary<int, List<int[]>>();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length - 1; j++)
+                {
+                    int product = arr[i] * arr[j];
+
+                    if (map.ContainsKey(product))
+                    {
+                        map[product].Add(new int[] { i, j });
+
+                        return map[product];
+                    }
+                    else
+                    {
+                        map.Add(product, new List<int[]>() { new int[] { i, j } });
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static bool checkParenthesis(string sequence)
+        {
+            Stack<string> stack = new Stack<string>();
+            Dictionary<char, string> dictionary = new Dictionary<char, string>()
+            {
+                { char.Parse("\""), "straightquotationmarks" },
+                { char.Parse("\'"), "singlestraightquotationmarks" }
+            };
+
+            foreach (char c in sequence)
+            {
+                if (dictionary.ContainsKey(c))
+                {
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(dictionary[c]);
+                    }
+                    else 
+                    {
+                        if (stack.Contains(dictionary[c]))
+                        {
+                            if (stack.Peek() == dictionary[c])
+                            {
+                                stack.Pop();
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else 
+                        {
+                            stack.Push(dictionary[c]);
+                        }
+                    }
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            if (stack.Count != 0) 
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+
+
+
+        private static void showFirstIndexesOfReapeatedNumbers(List<int> list)  
+        {
+            Dictionary<int, List<int>> valeIndexPair = new Dictionary<int, List<int>>();
+
+            int index = 0;
+            foreach (int i in list)
+            {
+                if (valeIndexPair.ContainsKey(i))
+                {
+                    valeIndexPair[i].Add(index);
+                }
+                else 
+                {
+                    valeIndexPair[i] = new List<int>() { index };
+                }
+                index++;
+            }
+        }
+    }
+}
